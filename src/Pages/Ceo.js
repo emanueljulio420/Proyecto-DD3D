1c86b51bdb5033899f925bde51d6fefd95eec8d7
emanueljulio420 <emanueljulio320@gmail.com>
2020-10-07 16:01:05 -0500

    Creacion del componente Header

---
+++ src/Pages/Ceo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header'
+
+function Ceo() {
+
+    return (
+
+        <Header />
+
+    )
+}
+
+export default Ceo;
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Ceo from './Pages/Ceo';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+ReactDOM.render(
+  <>
+  <Ceo />
+  </>,
+  document.getElementById('root')
+);
+
