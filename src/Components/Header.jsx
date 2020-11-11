4790a618b6931da10c5cd6f123ee82710d31f9b5
simonecha <simon.echavarria.corre4@gmil.com>
2020-11-11 15:16:40 -0500

    jiiiiiiii

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import './style.css'
 
-function Header() {
+   function Header (){
+   return (
 
-    return (
+        <div className="Header">
+            <header className="blog-header py-1">
+                <div className="row flex-nowrap justify-content-between align-items-center">
+                    
+                    <div className="col-12 text-center">
+                        <table>
+                            <tr>
+                                <td> imagen </td>
+                                <td> boton </td>
+                
+                            </tr>
+                        </table>
+                    </div>
+                   
+                </div>
+            </header>
 
-    <body>
+        </div>
 
-        <h1>hola</h1>
-    
-    </body>
-
-    )
-
+    );
 }
 
 export default Header;
